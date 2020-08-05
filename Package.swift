59e73eae29788efaaa08676757e8da5e04a5cfbd
Roman Podymov <podymfrombryansk@yandex.ru>
2020-08-05 20:47:55 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,6 +24,6 @@ let package = Package(
         .testTarget(
             name: "PrettyColorsTests",
             dependencies: ["PrettyColors"],
-            path: "Tests/PrettyColorsTests"),
+            path: "Tests"),
     ]
 )
